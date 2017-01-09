b9668d8a503f9fc373fbf9337f57f6e1fb1502ac
litian54 <tianli8954@gmail.com>
2017-01-09 01:38:38 -0800

    Add gitignore

--- install/git.sh
+++ install/git.sh
@@ -6,3 +6,6 @@ echo "============================================"
 
 gitconfig="$DOTFILES_DIR/home/git/gitconfig"
 [[ -f ${gitconfig} ]] && ln -nfs ${gitconfig} $HOME/.gitconfig
+
+gitignore="$DOTFILES_DIR/home/git/gitignore"
+[[ -f ${gitignore} ]] && ln -nfs ${gitignore} $HOME/.gitignore
