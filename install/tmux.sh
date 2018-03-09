8a88ed60362631c89fdf5cfb02f4802457ab0696
litian54 <tianli8954@gmail.com>
2018-03-09 13:37:44 +0800

    Use gpakosz/.tmux as the tmux configuration

--- install/tmux.sh
+++ install/tmux.sh
@@ -4,5 +4,15 @@ echo "============================================"
 echo "Setup tmux config..."
 echo "============================================"
 
-tmux="$DOTFILES_DIR/home/tmux/tmux.conf"
-[[ -f ${tmux} ]] && ln -nfs ${tmux} $HOME/.tmux.conf
+tmux_dir="$DOTFILES_DIR/home/tmux"
+[[ -f ${tmux_dir}/.tmux.conf ]] && ln -nfs ${tmux_dir}/.tmux.conf $HOME/.tmux.conf
+[[ -f ${tmux_dir}/.tmux.conf.local ]] && ln -nfs ${tmux_dir}/.tmux.conf.local $HOME/.tmux.conf.local
+
+cat << 'EOF' >> ${HOME}/.tmux.conf.local
+set -g history-limit 10000
+set -g status-keys vi
+set -g mode-keys vi
+setw -g mode-keys vi
+bind e setw synchronize-panes on
+bind E setw synchronize-panes off
+EOF
